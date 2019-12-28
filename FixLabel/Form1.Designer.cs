ecfa66af4580aa837cc22156a0c66cfe4b5c318d
duytuit <38493145+duytuit@users.noreply.github.com>
2019-12-28 16:03:02 +0700

    28/12/2019

---
+++ FixLabel/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FixLabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPartNo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPartNo
+            // 
+            this.txtPartNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPartNo.Location = new System.Drawing.Point(49, 83);
+            this.txtPartNo.Name = "txtPartNo";
+            this.txtPartNo.Size = new System.Drawing.Size(185, 45);
+            this.txtPartNo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập mã hàng:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Xử Lý Lỗi Tạo Tem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPartNo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPartNo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FixLabel/Form1.cs
@@ -0,0 +1,158 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FixLabel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            if(0<txtPartNo.Text.Length&&txtPartNo.Text.Length==9)
+            {
+                using (var ParadigmDB=new Entities())
+                {
+                   int CheckPartNo= ParadigmDB.DATA0050.Where(x=>x.CUSTOMER_PART_NUMBER.Contains(txtPartNo.Text)).Count();
+
+                    if(CheckPartNo>0)
+                    {
+                        int countPartNo = ParadigmDB.T_BARCODE_FIXPROD_ID.Where(x => x.CUST_PART_FIX==txtPartNo.Text).Count();
+
+                        if (countPartNo==0)
+                        {
+                            OracleConnection oraConn = new OracleConnection();
+                            string connString = "User Id=ADMIN; Password=demo; Data Source=fnsystem:1521/live";
+                            oraConn.ConnectionString = connString;
+                            oraConn.Open();
+                            OracleCommand cmd = new OracleCommand();
+                           
+                            cmd.Connection = oraConn;
+                            OracleTransaction myTrans = oraConn.BeginTransaction();
+                            cmd.Transaction = myTrans;
+                            
+                            try
+                            {
+                                string sqlInsert = "insert into T_BARCODE_FIXPROD_ID (cust_part_fix) values('" + txtPartNo.Text + "')";
+                                cmd.CommandText = sqlInsert;
+                                cmd.ExecuteNonQuery();
+                                myTrans.Commit();
+                                cmd.Dispose();
+                                button1.Enabled = true;
+                                txtPartNo.Text = null;
+                                MessageBox.Show("Thành Công!");
+                            }
+                            catch 
+                            {
+                                myTrans.Rollback();
+                                MessageBox.Show("Thất Bại!");
+                            }
+                            finally
+                            {
+                                oraConn.Close();
+                            }
+                        }
+                        else  if(countPartNo == 1)
+                        {
+                            OracleConnection oraConn = new OracleConnection();
+                            string connString = "User Id=ADMIN; Password=demo; Data Source=fnsystem:1521/live";
+                            oraConn.ConnectionString = connString;
+                            oraConn.Open();
+                            OracleCommand cmd = new OracleCommand();
+                            cmd.Connection = oraConn;
+                            OracleTransaction myTrans = oraConn.BeginTransaction();
+                            cmd.Transaction = myTrans;
+
+                            try
+                            {
+                                string sqlDelete = "delete from T_BARCODE_FIXPROD_ID where CUST_PART_FIX ='" + txtPartNo.Text + "'";
+                                cmd.CommandText = sqlDelete;
+                                cmd.ExecuteNonQuery();
+                                string sqlInsert = "insert into T_BARCODE_FIXPROD_ID (cust_part_fix) values('" + txtPartNo.Text + "')";
+                                cmd.CommandText = sqlInsert;
+                                cmd.ExecuteNonQuery();
+                                myTrans.Commit();
+                                cmd.Dispose();
+                                button1.Enabled = true;
+                                txtPartNo.Text = null;
+                                MessageBox.Show("Thành Công!");
+                            }
+                            catch
+                            {
+                                myTrans.Rollback();
+                                MessageBox.Show("Thất Bại!");
+                            }
+                            finally
+                            {
+                                oraConn.Close();
+                            }
+                        }
+                        else
+                        {
+                            OracleConnection oraConn = new OracleConnection();
+                            string connString = "User Id=ADMIN; Password=demo; Data Source=fnsystem:1521/live";
+                            oraConn.ConnectionString = connString;
+                            oraConn.Open();
+                            OracleCommand cmd = new OracleCommand();
+                            cmd.Connection = oraConn;
+                            OracleTransaction myTrans = oraConn.BeginTransaction();
+                            cmd.Transaction = myTrans;
+                            try
+                            {
+                                for (int i = 0; i < countPartNo; i++)
+                                {
+                                    string sqlDelete = "delete from T_BARCODE_FIXPROD_ID where CUST_PART_FIX ='" + txtPartNo.Text + "'";
+                                    cmd.CommandText = sqlDelete;
+                                    cmd.ExecuteNonQuery();
+                                }
+                                string sqlInsert = "insert into T_BARCODE_FIXPROD_ID (cust_part_fix) values('" + txtPartNo.Text + "')";
+                                cmd.CommandText = sqlInsert;
+                                cmd.ExecuteNonQuery();
+                                myTrans.Commit();
+                                cmd.Dispose();
+                                button1.Enabled = true;
+                                txtPartNo.Text = null;
+                                MessageBox.Show("Thành Công!");
+                            }
+                            catch
+                            {
+                                myTrans.Rollback();
+                                MessageBox.Show("Thất Bại!");
+                            }
+                            finally
+                            {
+                                oraConn.Close();
+                            }
+
+
+                        }
+                    }else
+                    {
+                        button1.Enabled = true;
+                        txtPartNo.Text = null;
+                        MessageBox.Show("Mã hàng không tồn tại!");
+                    }
+                }
+            }
+            else
+            {
+                txtPartNo.Text = null;
+                button1.Enabled = true;
+                MessageBox.Show("Nhập tối thiểu 9 ký tự!");
+            }
+        }
+    }
+}
